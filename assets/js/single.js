ce381ad59cd28e2e371042c18ae6d58cd42476e9
Joseph Darville <joseph.darville9@gmail.com>
2022-05-27 03:19:08 -0400

    changed js file name homepage

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,13 +9,17 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
           response.json().then(function(data) {
             displayIssues(data);
+
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
           });
         }
         else {
           alert("There was a problem with your request!");
         }
       });
-
 };
 
 var displayIssues = function(issues) {
@@ -48,5 +53,16 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl)
     }
 };
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+};
   
-  getRepoIssues("Jdarville9/get-it-done");
+  getRepoIssues("facebook/react");
